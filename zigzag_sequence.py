e85c9f5d8d6c39aaae5b25fae6d20a89c2566b7b
Srividya23 <srividya.sekar23@gmail.com>
2022-07-20 14:59:56 -0400

    Function to transforma an array into a zigzag sequence

---
+++ zigzag_sequence.py
@@ -0,0 +1,33 @@
+# Given an array of n distinct integers, transform the array into a zig zag sequence by permuting the array elements. A sequence will be called a zig zag sequence if the first k elements in the sequence are in increasing order and the last k elements are in decreasing order, where k = (n+1)/2. The goal is to find the lexicographically smallest zig zag sequence of the given array.
+
+# Example:
+# Input: a = [2,3,1,5,4]
+# Output: a = [1,4,5,3,2]
+
+def findZigZagSequence(a, n):
+    a.sort()
+    mid = int((n + 1)/2)-1
+    a[mid], a[n-1] = a[n-1], a[mid]
+
+    st = mid + 1
+    ed = n - 2
+    while(st <= ed):
+        a[st], a[ed] = a[ed], a[st]
+        st = st + 1
+        ed = ed - 1
+
+    for i in range (n):
+        if i == n-1:
+            print(a[i])
+        else:
+            print(a[i], end = ' ')
+    return
+
+test_cases = int(input())
+for cs in range (test_cases):
+    n = int(input())
+    a = list(map(int, input().split()))
+    findZigZagSequence(a, n)
+
+
+
