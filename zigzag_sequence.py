7e365e9284169e4b4bec558dec4f7512abd32c19
Srividya23 <srividya.sekar23@gmail.com>
2022-07-20 15:01:02 -0400

    Function to transform an array into a zigzag sequence

--- zigzag_sequence.py
+++ zigzag_sequence.py
@@ -1,4 +1,5 @@
-# Given an array of n distinct integers, transform the array into a zig zag sequence by permuting the array elements. A sequence will be called a zig zag sequence if the first k elements in the sequence are in increasing order and the last k elements are in decreasing order, where k = (n+1)/2. The goal is to find the lexicographically smallest zig zag sequence of the given array.
+# Given an array of n distinct integers, transform the array into a zig zag sequence by permuting the array elements. A sequence will be called a zig zag sequence if the first k elements in the sequence are in increasing order and the last k elements are in decreasing order, where k = (n+1)/2. 
+# The goal is to find the lexicographically smallest zig zag sequence of the given array.
 
 # Example:
 # Input: a = [2,3,1,5,4]
